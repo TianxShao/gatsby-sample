0d2159f1d2960f958fb1dc89b0bf15c982e667f9
TianxShao <105902438+TianxShao@users.noreply.github.com>
2023-07-30 09:07:01 -0400

    Add Layout

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const About = () => {
+    return (
+        <Layout pageTitle="About Team Gatsby">
+            
+        </Layout>
+    )
+}
+
+export default About
